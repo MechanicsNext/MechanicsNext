5d19b136e4c7aa45a750df6fb6b290ac9cb6548f
Harshad Paranjape <harshad@login2.stampede.tacc.utexas.edu>
2017-03-11 13:40:08 -0600

    Added comments to HEDM_preprocessing programs

--- Experimentation/HEDM_preprocessing/spot_maxima_finder.py
+++ Experimentation/HEDM_preprocessing/spot_maxima_finder.py
@@ -1,9 +1,14 @@
 ###############################################################################
-# Diffraction-toolkit (https://github.com/hmparanjape/diffraction-toolkit)
-# GE Pre-processor Library
+# MechanicsNext: ff-HEDM Spot Segmentation Program
+# (https://github.com/MechanicsNext/MechanicsNext/blob/master/Experimentation/HEDM_preprocessing/)
 #
-# Find local maxima of spots in HEDM diffraction patterns.
-# Return spot shape, size data.
+# Find local maxima of spots in ff-HEDM diffraction patterns.
+# Return spot position, shape, and size data.
+#
+# USAGE:
+#  python spot_maxima_finder.py config_file.yml
+#
+# See examples_spot_maxima_finder/ folder for example configurations.
 #
 # Written by Harshad Paranjape (hparanja@mines.edu) and contributors.
 # All rights reserved.
@@ -17,14 +22,15 @@ import os
 import sys
 import time
 import warnings
-
+# YAML and heXRD imports to read heXRD-like configuration files
 import yaml
-
 from hexrd import config
+# Main program routines
 from ge_processor.ge_pre_processor import *
 
+# Run the program.
 if __name__ == '__main__':
-    # Read args
+    # Read command line arguments
     if len(sys.argv) < 2:
         print 'USAGE: python spot_maxima_finder.py config.yml'
         sys.exit(1)
@@ -34,7 +40,8 @@ if __name__ == '__main__':
     else:
         cfg_file = sys.argv[1]
 
-    # Setup logger
+    # Setup logger. Information about the progress of the program will be
+    # printed to the logger.
     log_level = logging.DEBUG
     logger = logging.getLogger('hexrd')
     logger.setLevel(log_level)
@@ -46,15 +53,27 @@ if __name__ == '__main__':
     # load the configuration settings
     cfgs = config.open(cfg_file)
 
-    # cfg is a list. We will loop over each cfg set.
+    # cfg is a list. We will loop over each cfg set. i.e., we can in principal have multiple
+    # spot cleanup jobs in a single configuration file.
     for cfg in cfgs:
-    	# Initialize the GE pre-processor
+    	# Initialize the GE pre-processor object. This object wraps all
+        # spot information.
     	gepp = GEPreProcessor(cfg=cfg, logger=logger)
     	# Start analysis
     	logger.info('=== begin image-smoothing ===')
-    	# Load the GE2 data
+    	# Load the GE2 data. This will take several minutes, since a typical ff-HEDM
+        # dataset consists of tens of GB of data.
     	gepp.load_data()
-    	# ID blobs and local maxima, and optionally print a text file with local maxima positions,
-        # PNG files and GE2 file with cleaned-up data.
+        # For spot segmentation, following terminology is used:
+        # Raw data or GE2 data: 3D array consisting of X, Y, and Omega dimensions that stores intensity information.
+        # Blob: Any volume in the raw data that has intensity above a threshold. This may contain several overlapping spots.
+        # Local maxima: Local intensity maxima in a blob.
+        # Spot: A region inside a blob that has only one intensity maximum inside it.
+        #
+    	# Identify blobs and local maxima, and optionally print:
+        # 1. A text file with local maxima positions,
+        # 2. PNG files with diagnostic images showing blobs, local maxima positions, and spots identified using the watershed algo.
+        # 3. GE2 file with cleaned-up (segmented) data.
     	gepp.find_blobs()
+    # END of program
 
